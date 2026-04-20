167cb57a79de8f8cd5718ad3a94ad181a540d218
SansamKim <dm34941@gmail.com>
2026-04-20 12:33:40 +0900

    추상화와 캡슐화

---
+++ cpp_sample/ch7/ch7-1/bank_access_error.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+class bank {
+private:
+    int safe;       // 금고
+    
+public: bank();     // 기본 생성자
+    void use_counter(int _in, int _out);    // 입출금 창구 함수
+};
+
+bank::bank() {
+    safe = 1000;
+    cout << "최초 금고 : " << safe << endl;
+    cout << endl;
+}
+
+void bank::use_counter(int _in, int _out) {
+    safe += _in;
+    safe -+ _out;
+
+    cout << "입금 : " << _in << endl;
+    cout << "출금 : " << _out <<endl;
+    cout << "금고 : " << safe << endl;
+    cout << endl;
+}
+
+int main()
+{
+    bank my_bank;   // my_bank 인스턴스 생성
+
+    my_bank.use_counter(0, 20);
+    my_bank.use_counter(50, 0);
+    my_bank.use_counter(100, 50);
+
+ // my_bank.safe -= 100     // private으로 지정된 금고에 직접 접근하여 인출 시도
+
+    return 0;
+}
